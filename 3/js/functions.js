a54cb6c15bcaf07830952339753bd3e283935021
Keks <keks@htmlacademy.ru>
2024-02-19 11:30:08 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,46 @@
+//Функция для проверки длины строки.
+// eslint-disable-next-line no-unused-vars
+function stringLength (string, maxLength) {
+  if (string.length <= maxLength) {
+    return true;
+  }
+
+  return false;
+}
+
+//Функция для проверки, является ли строка палиндромом
+// eslint-disable-next-line no-unused-vars
+function stringPalindrome (string) {
+  string = string.replaceAll(' ', '').toLowerCase();
+  let newString = '';
+
+  for (let i = string.length - 1; i >= 0; i--){
+    // eslint-disable-next-line no-unused-vars
+    newString += string[i];
+  }
+
+  if (newString === string) {
+    return true;
+  }
+
+  return false;
+}
+
+//Функция возарата числа
+// eslint-disable-next-line no-unused-vars
+function returnNumber (string) {
+  let newNumberString = '';
+  string = string.toString();
+  for (let i = 0; i <= string.length; i++) {
+    const number = parseInt(string[i], 10);
+
+    if (!isNaN(number)) {
+      // eslint-disable-next-line no-unused-vars
+      newNumberString += number;
+    }
+  }
+
+  newNumberString = parseInt(newNumberString, 10);
+  // eslint-disable-next-line no-undef
+  return newNumberString;
+}
